18fc8cda061755933de6533d222d69f620896e49
Arthur Correnson <arthur.correnson@gmail.com>
2019-05-15 22:33:08 +0200

    Restructure the project to add setup script

--- logik/parser.py
+++ logik/parser.py
@@ -1,4 +1,4 @@
-from lexer import *
+from . lexer import *
 
 
 def priority(char):
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='logik',
+      version='0.1',
+      description='A simple way to interact with propositional logic in python',
+      url='http://github.com/storborg/funniest',
+      author='Arthur Correnson',
+      author_email='arthur.correnson@gmail.com',
+      license='MIT',
+      packages=['logik'],
+      scripts=['bin/logik'],
+      zip_safe=False)
