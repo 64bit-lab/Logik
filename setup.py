2ada2d7af3c987312df70b20292619dd041a2f9d
Arthur Correnson <arthur.correnson@gmail.com>
2019-05-15 22:39:37 +0200

    Add installation details in README

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='logik',
       version='0.1',
       description='A simple way to interact with propositional logic in python',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/64bit-lab/Logik',
       author='Arthur Correnson',
       author_email='arthur.correnson@gmail.com',
       license='MIT',
