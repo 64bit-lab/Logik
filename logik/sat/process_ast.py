4d9f1bc1c942c10f977887b1d082edffa686cddf
Arthur Correnson <arthur.correnson@gmail.com>
2019-09-14 15:38:23 +0200

    add cnf conversion function

--- logik/sat/process_ast.py
+++ logik/sat/process_ast.py
@@ -9,20 +9,120 @@ def remove_implications(ast):
     """
     if len(ast) == 3:
         op, oper1, oper2 = ast
+        oper1 = remove_implications(oper1)
+        oper2 = remove_implications(oper2)
         if op == '->':
-            oper1 = remove_implications(oper1)
-            oper2 = remove_implications(oper2)
             return ('ou', ('non', oper1), oper2)
         else:
             return ast
     return ast
 
 
-def remove_and(ast):
+def _is_node_op(ast, op):
+    return ast[0] == op
+
+
+def distribute_or(ast):
+    """
+    @brief      Distributes or on and if needed.
+    
+    @param      ast   The ast
+    
+    @return     another ast
+    """
+    
+    assert not _is_node_op(ast, '->'), \
+        "Or can only be distributed on implication free AST"
+    assert ast is not None, "Empty ast"
+
+    if _is_node_op(ast, 'or'):
+        _, exprA, exprB = ast
+        exprA = distribute_or(exprA)
+        exprB = distribute_or(exprB)
+
+        if  _is_node_op(exprB, 'and'):
+            _, exprC, exprD = exprB
+            exprC = distribute_or(exprC)
+            exprD = distribute_or(exprD)
+
+            left  = distribute_or(('or', exprA, exprC))
+            right = distribute_or(('or', exprA, exprD))
+            return ('and', left, right)
+
+        if _is_node_op(exprA, 'and'):
+            _, exprC, exprD = exprA
+            exprC = distribute_or(exprC)
+            exprD = distribute_or(exprD)
+
+            left  = distribute_or(('or', exprC, exprB))
+            right = distribute_or(('or', exprD, exprB))
+            return ('and', left, right)
+
+    if len(ast) == 2:
+        return ast
+
     if len(ast) == 3:
-        op, oper1, oper2 = ast
-        oper1 = remove_and(oper1)
-        oper2 = remove_and(oper2)
-        if op == 'et':
-            oper1 = distribute(oper1)
-            oper2 = distribute(oper2)
+        a, b, c = ast
+        return (a, distribute_or(b), distribute_or(c))
+
+
+def remove_negations(ast):
+    """
+    @brief      Removes all negations.
+    
+    @param      ast   The ast
+    
+    @return     another ast
+    """
+
+    assert not _is_node_op(ast, '->'), \
+        "Negations can only be removed on implication free AST"
+    assert ast is not None, "Empty ast"
+
+    if _is_node_op(ast, 'non'):
+        _, exprA = ast
+        if _is_node_op(exprA, 'or'):
+            _, exprB, exprC = exprA
+            exprB = remove_negations(('non', exprB))
+            exprC = remove_negations(('non', exprC))
+            return ('and', exprB, exprC)
+
+        if _is_node_op(exprA, 'and'):
+            _, exprB, exprC = exprA
+            exprB = remove_negations(('non', exprB))
+            exprC = remove_negations(('non', exprC))
+            return ('or', exprB, exprC)
+    
+    if len(ast) == 3:
+        op, A, B = ast
+        A = remove_negations(A)
+        B = remove_negations(B)
+        return (op, A, B)
+
+    if len(ast) == 2:
+        return ast
+
+
+def convert_to_cnf(ast):
+    """
+    @brief      Convert an ast into Conjuntive Normal Form
+    
+    @param      ast   The ast
+    
+    @return     { description_of_the_return_value }
+    """
+    ast = remove_implications(ast)
+    ast = remove_negations(ast)
+    ast = distribute_or(ast)
+
+    return ast
+
+
+
+# print(distribute_or(('or', ('and', ('value', 'a'), ('value', 'b')), ('value', 'c'))))
+# print(distribute_or(('or', 
+#     ('value', 'a'), 
+#     ('or',  ('value', 'b'),
+#             ('and', ('value', 'c'), ('value', 'd'))))))
+
+# print(remove_negations(('non', ('and', ('value', 'a'), ('value', 'b')))))
